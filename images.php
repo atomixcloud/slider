85339c158a7206633b6dce3ae7ad24a1fc1f3700
Kirubakaran Shanmugam <kribakarans@gmail.com>
2025-02-17 01:22:19 +0530

    Enhanced code

--- images.php
+++ images.php
@@ -1,6 +1,15 @@
 <?php
-$images = array_values(array_filter(scandir("."), function($file) {
-    return preg_match('/\.(jpg|jpeg|png|gif)$/i', $file);
+$dir = "."; // Current directory
+$base_url = "http://" . $_SERVER['HTTP_HOST'] . dirname($_SERVER['SCRIPT_NAME']) . "/";
+
+$images = array_values(array_filter(scandir($dir), function($file) use ($dir) {
+    return is_file("$dir/$file") && preg_match('/\.(jpg|jpeg|png|gif)$/i', $file);
 }));
+
+$images = array_map(function($file) use ($base_url) {
+    return $base_url . $file; // Full URL for the images
+}, $images);
+
+header('Content-Type: application/json');
 echo json_encode($images);
 ?>
